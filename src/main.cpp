f6b37d3e7446dbf510ec770524ca82a00c183372
nguyenasd132 <79546352+nguyenasd132@users.noreply.github.com>
2025-05-18 15:41:41 +0700

    xoa DanhSachQuanNhan, them Si Quan, Binh Si

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,14 +9,24 @@ using namespace std;
 
 class QuanNhan {
     string maSo, hoTen, capBac, donVi, queQuan, ngaySinh, ngayNhapNgu;
-
+    vector<string> DSNhiemVu;
 public:
+    QuanNhan() {
+        maSo = "";
+        hoTen = "";
+        capBac = "";
+        donVi = "";
+        ngaySinh = "";
+        ngayNhapNgu = "";
+    }
     QuanNhan(string ms, string ht, string cb, string dv, string qq, string ns, string nnn)
         : hoTen(ht), capBac(cb), donVi(dv), queQuan(qq), maSo(ms), ngaySinh(ns), ngayNhapNgu(nnn) {}
+    QuanNhan(const QuanNhan& qn) {}
 
-    void hienThiThongTin() const {
+    ~QuanNhan() {}
+    virtual void hienThiThongTin() const {
         cout << "Ma so: " << maSo << ", Ho ten: " << hoTen << endl;
-        cout << "Cap bac: " << capBac << ", Don vi: " << donVi << endl;
+        cout << "Cap bac: " << getCapBac() << ", Don vi: " << donVi << endl;
         cout << "Que quan: " << queQuan << ", ngay sinh: " << ngaySinh << endl;
         cout << "Ngay nhap ngu: " << ngayNhapNgu << endl;
     }
@@ -28,166 +37,107 @@ public:
     string getQueQuan() const { return queQuan; }
     string getNgaySinh() const { return ngaySinh; }
     string getNgayNhapNgu() const { return ngayNhapNgu; }
+    vector<string> getDSNhiemVu() const { return DSNhiemVu; }
+    void setMaSo(string ms) { maSo = ms; }
+    void setHoTen(string ht) { hoTen = ht; }
+    void setCapBac(string cb) { capBac = cb; }
+    void setDonVi(string dv) { donVi = dv; }
+    void setQueQuan(string qq) { queQuan = qq; }
+    void setNgaySinh(string ns) { ngaySinh = ns; }
+    void setNgayNhapNgu(string nnn) { ngayNhapNgu = nnn; }
+    void setNhiemVu(string NV) {DSNhiemVu.push_back(NV);}
+    bool baoCaoNhiemVu(string NV) {
+        string x; bool flag = false;
+        cout << "Bao cao tien do nhiem vu:" << endl;
+        cout << "Ban da hoan thanh nhiem vu (roi/chua): "; cin >> x;
+        if (x == "roi" || x == "Roi")
+            flag = true;
+        return flag;
+    }
+    int maHoaCapBac() {
+        if (capBac == "Binh nhi") return 1;
+        if (capBac == "Binh nhat") return 2;
+        if (capBac == "Ha si") return 3;
+        if (capBac == "Trung si") return 4;
+        if (capBac == "Thuong si") return 5;
+        if (capBac == "Thieu uy") return 6;
+        if (capBac == "Trung uy") return 7;
+        if (capBac == "Thuong uy") return 8;
+        if (capBac == "Dai uy") return 9;
+        if (capBac == "Thieu ta") return 10;
+        if (capBac == "Trung ta") return 11;
+        if (capBac == "Thuong ta") return 12;
+        if (capBac == "Dai ta") return 13;
+        return 0;
+    }
+};
+class SiQuan : public QuanNhan {
+    DanhSachQuanNhan donViQuanLy;
+    public:
+    SiQuan(): QuanNhan(), donViQuanLy(NULL) {}
+    SiQuan(string ms, string ht, string cb, string dv, string qq, string ns, string nnn,DanhSachQuanNhan dvql)
+        : QuanNhan(), donViQuanLy(dvql) {}
+    SiQuan(const SiQuan& q): QuanNhan(q), donViQuanLy(q.donViQuanLy) {}
 
+    ~SiQuan() {}
+    void getDonViQuanLy() {
+        if (donViQuanLy != NULL) {
+            cout << "Si Quan khong quan ly don vi nao" << endl;
+            return;
+        }
+        cout << donViQuanLy.getTenDonVI() << endl;
+    }
+    void hienThiThongTin() const {
+        cout << "---Thong Tin Si Quan---" << endl;
+        cout << "DonViQuanLy: " << donViQuanLy.getTenDonVi() << endl;
+        QuanNhan::hienThiThongTin();
+        cout << "-----------------------" << endl;
+    }
+    void giaoNV(string MSQN, string nhiemVu) {
+        bool flag = false;
+        for (QuanNhan qn : donViQuanLy) {
+            if (qn.getMaSo() == MSQN) {
+                flag = true;
+                if (maHoaCapBac() <= qn.maHoaCapBac()) {
+                    cout << "Ban khong co quyen ra lenh cho cap tren" << endl;
+                    return;
+                }
+                qn.setNhiemVu(nhiemVu);
+            }
+            cout << "Da giao nhiem vu cho binh si " << qn.getMaSo() <<'.' << endl;
+        }
+        if (!flag) {
+            cout << "Si Quan khong co quyen giao nhiem vu cho quan nhan nay" << endl;
+        }
+    }
+    void kiemTraNVQN(string MSQN) {
+        bool flag = false;
+        for (QuanNhan qn : donViQuanLy) {
+            if (qn.getMaSo() == MSQN) {
+                flag = true;
+                for (string NV : qn.getDSNhiemVu()) {
+                    cout << NV << ": ";
+                    if (qn.baoCaoNhiemVu()) cout << "Da hoan thanh" << endl;
+                    else cout << "chua hoan thanh" << endl;
+                }
+            }
+            cout << "Da giao nhiem vu cho quan nhan " << qn.getMaSo() <<'.' << endl;
+        }
+        if (!flag) {
+            cout << "Si Quan khong co quyen kiem tra nhiem vu cua quan Nhan nay" << endl;
+        }
+    }
+};
+class BinhSi: public QuanNhan {
+    void hienThiThongTin() const {
+        cout << "--------Binh Si--------" << endl;
+        QuanNhan::hienThiThongTin();
+        cout << "-----------------------" << endl;
+    }
 };
 
 
-class DanhSachQuanNhan {
-    vector<QuanNhan> danhSach;
-
-public:
-    DanhSachQuanNhan() {}
-    DanhSachQuanNhan(const vector<QuanNhan>& ds) {
-        danhSach = ds;
-    }
-    void themQuanNhan(const QuanNhan& qn) {
-        danhSach.push_back(qn);
-    }
-    vector<QuanNhan> getDanhSach() {
-        return danhSach;
-    }
-    void Duyet() const {
-        cout << "\n--- Danh sach quan nhan ---\n";
-        for (const QuanNhan& qn : danhSach) {
-            qn.hienThiThongTin();
-            cout << "-------------------------\n";
-        }
-    }
-
-    QuanNhan* getQuanNhan(string maSo) {
-        for (QuanNhan& qn : danhSach) {
-            if (qn.getMaSo() == maSo)
-                return &qn;
-        }
-        return nullptr;
-    }
-
-    void ghiFile(const string& tenFile) const {
-        ofstream out(tenFile);
-        for (const QuanNhan& qn : danhSach) {
-            out << qn.getMaSo() << "," << qn.getHoTen() << "," << qn.getCapBac()
-                << "," << qn.getDonVi() << "," << qn.getQueQuan() << "," << qn.getNgaySinh() << "," << qn.getNgayNhapNgu() << "\n";
-        }
-        out.close();
-    }
-
-    void docFile(const string& tenFile) {
-        ifstream in(tenFile);
-        string line;
-        while (getline(in, line)) {
-            stringstream ss(line);
-            string ma_so, ten, capBac, donVi, queQuan, ngaysinh, ngayNhapNgu;
-            if (line.empty()) {
-                continue;
-            }
-            getline(ss, ma_so, ',');
-            getline(ss, ten, ',');
-            getline(ss, capBac, ',');
-            getline(ss, donVi, ',');
-            getline(ss, queQuan, ',');
-            getline(ss, ngaysinh, ',');
-            getline(ss, ngayNhapNgu);
-            danhSach.emplace_back(ma_so, ten, capBac, donVi, queQuan, ngaysinh, ngayNhapNgu);
-
-        }
-        in.close();
-    }
-    void thongKeTheoCapBac() const {
-        cout << "\nThong ke theo cap bac:\n";
-        vector<string> CapBac;
-        for (const QuanNhan& qn : danhSach) {
-            bool found = false;
-            for (const string& x : CapBac) {
-                if (x == qn.getCapBac()) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                CapBac.push_back(qn.getCapBac());
-            }
-        }
-
-        for (const string& x : CapBac) {
-            int count = 0;
-            for (const QuanNhan& qn : danhSach) {
-                if (qn.getCapBac() == x) {
-                    count++;
-                }
-            }
-            cout << x << ": " << count << " nguoi\n";
-        }
-    }
-    vector<QuanNhan> getDSSiQuan(const string& capbac) {
-        vector<QuanNhan> DStmp;
-        for (const QuanNhan& qn : danhSach) {
-            if (capbac == qn.getCapBac()) {
-                DStmp.push_back(qn);
-            }
-        }
-        return DStmp;
-    }
-    void thongKeTheoDonVi() const {
-        cout << "\nThong ke theo don vi:\n";
-        vector<string> DonVi;
-        for (const QuanNhan& qn : danhSach) {
-            bool found = false;
-            for (const string& x : DonVi) {
-                if (x == qn.getDonVi()) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found) {
-                DonVi.push_back(qn.getDonVi());
-            }
-        }
-
-        for (const string& x : DonVi) {
-            int count = 0;
-            for (const QuanNhan& qn : danhSach) {
-                if (qn.getDonVi() == x) {
-                    count++;
-                }
-            }
-            cout << x << ": " << count << " nguoi\n";
-        }
-    }
-    void duyetDonVi(const string& tuKhoa) const {
-        cout << "\nKet qua tim kiem: \n";
-        bool found = false;
-        for (const QuanNhan& qn : danhSach) {
-            if (qn.getHoTen().find(tuKhoa) != string::npos ||
-                qn.getDonVi().find(tuKhoa) != string::npos ||
-                qn.getQueQuan().find(tuKhoa) != string::npos)
-            {
-                qn.hienThiThongTin();
-                cout << "-------------------------\n";
-                found = true;
-            }
-        }
-        if (!found) {
-            cout << "Khong tim thay ket qua nao.\n";
-        }
-    }
-
-    bool isEmpty() const {
-        return danhSach.empty();
-    }
-};
 
 int main() {
-    DanhSachQuanNhan DSQN;
-    DSQN.docFile("quannhan.txt");
-    DSQN.thongKeTheoCapBac();
-    // DSQN.Duyet();
-    // DSQN.duyetDonVi("Tieu đoan 1");
-    DSQN.thongKeTheoDonVi();
-    /*DanhSachQuanNhan ThieuUy(DSQN.getDSSiQuan("Thieu uy"));
-    ThieuUy.Duyet();
-    ThieuUy.ghiFile("DanhSachThieuUy.txt");
-    DSQN.ghiFile("output.txt");*/
     return 0;
 }
-=======
->>>>>>> 017bbee618118199206ede047954f27a824abe65
